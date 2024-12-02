f6089fb1f54124c8d438c18999adb74ec854dc76
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-12-02 16:13:38 +0100

    ADD: Creazione del progetto react-blog

---
+++ src/data/posts.js
@@ -0,0 +1,43 @@
+export const posts = [
+  {
+    title: "Ciambellone",
+    content:
+      "Sarà che una volta le cose erano più semplici, ma erano anche molto buone. Come le crostate, i biscotti o il ciambellone che la nonna preparava anche all'ultimo sapendo che sareste passati per la merenda: uova, zucchero e farina...",
+    image: "ciambellone.jpeg",
+    tags: ["Dolci", "Torte", "Ricette vegetariane", "Ricette al forno"],
+  },
+  {
+    title: "Cracker alla barbabietola",
+    content:
+      "I cracker alla barbabietola sono uno snack stuzzicante e originale da preparare in casa utilizzando ingredienti semplici e genuini...",
+    image: "cracker_barbabietola.jpeg",
+    tags: ["Antipasti", "Ricette vegetariane", "Ricette al forno"],
+  },
+  {
+    title: "Pasta barbabietola e gorgonzola",
+    content:
+      "La nostra ricetta della pasta barbabietola e gorgonzola vuole ricreare in questo primo piatto un abbinamento appetitoso...",
+    image: "pasta_barbabietola.jpeg",
+    tags: ["Primi piatti", "Ricette vegetariane"],
+  },
+  {
+    title: "Pane fritto dolce",
+    content:
+      "Il pane fritto dolce è la versione più antica dell'odierno french toast! Una deliziosa ricetta antispreco...",
+    image: "pane_fritto_dolce.jpeg",
+    tags: ["Dolci", "Dolci veloci", "Ricette veloci", "Ricette vegetariane"],
+  },
+  {
+    title: "Torta paesana",
+    content:
+      "La torta paesana è un dolce di origine lombarda e precisamente della Brianza. È un dolce di origine contadina, dalle infinite varianti...",
+    image: "torta_paesana.jpeg",
+    tags: [
+      "Dolci",
+      "Dolci al cioccolato",
+      "Torte",
+      "Ricette vegetariane",
+      "Ricette al forno",
+    ],
+  },
+];
