370504d247cf41fddcb70aa7479bd8679bbca45f
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-12-02 16:14:24 +0100

    ADD: implementazione card.jsx e stili

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./Card.module.css";
+
+export default function Card({ title, content, image, tags }) {
+  return (
+    <div className={styles.card}>
+      <img src={image} alt={title} className={styles.image} />
+      <div className={styles.content}>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.description}>{content}</p>
+        <div className={styles.tags}>
+          {tags.map((tag, index) => (
+            <span key={index} className={styles.tag}>
+              {tag}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
